b7cff34fc2feee04d1332c89c3b402ef38f66410
Garc Onierre <onierregarc@gmail.com>
2025-02-14 15:03:49 +0300

    Cosmetic changes

--- src/utils/sampleAudio.ts
+++ src/utils/sampleAudio.ts
@@ -2,13 +2,13 @@ export const sampleAudios = {
   '0-3': {
     boy: {
       id: 'sample_boy_0_3',
-      title: 'Minik Astronot Ali',
+      title: "Minik Astronot Ali",
       audioFile: require('../assets/sounds/samples/sample_boy_0_3.mp3'),
       duration: '2:30',
     },
     girl: {
       id: 'sample_girl_0_3',
-      title: 'Prenses Ela'nın Sihirli Bahçesi',
+      title: "Prenses Ela'nın Sihirli Bahçesi",
       audioFile: require('../assets/sounds/samples/sample_girl_0_3.mp3'),
       duration: '2:15',
     },
@@ -16,13 +16,13 @@ export const sampleAudios = {
   '3-6': {
     boy: {
       id: 'sample_boy_3_6',
-      title: 'Kaşif Kerem'in Orman Macerası',
+      title: "Kaşif Kerem'in Orman Macerası",
       audioFile: require('../assets/sounds/samples/sample_boy_3_6.mp3'),
       duration: '3:00',
     },
     girl: {
       id: 'sample_girl_3_6',
-      title: 'Zeynep'in Gökkuşağı Okulu',
+      title: "Zeynep'in Gökkuşağı Okulu",
       audioFile: require('../assets/sounds/samples/sample_girl_3_6.mp3'),
       duration: '2:45',
     },
